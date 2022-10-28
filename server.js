714a2d1130db2544d432b5ba5428b60aa0b0373b
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-27 21:55:40 -0700

    download express package and add api route in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,7 @@ app.use(express.Router.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/api', apiRoutes);
 app.use("/", htmlRoutes);
 
 app.get('/', (req, res) => {
