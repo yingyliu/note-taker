4a85db1c2adeff547dc6085862a5f9c4a0cad6d0
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-27 20:31:09 -0700

    basic express function setting

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.Router.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
