e3ece1f301f7eb6f00701337013836793483f849
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-27 21:02:16 -0700

    create html routes js and add app in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.use(express.Router.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/", htmlRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
